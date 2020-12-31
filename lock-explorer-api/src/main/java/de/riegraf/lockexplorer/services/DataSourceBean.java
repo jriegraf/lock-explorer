1b4d5e22233eaf767b211e69731b88223d05d649
Julian Riegraf <julianriegraf@web.de>
2020-12-31 15:18:06 +0100

    read database credentials from environment variables

---
+++ lock-explorer-api/src/main/java/de/riegraf/lockexplorer/services/DataSourceBean.java
@@ -0,0 +1,33 @@
+package de.riegraf.lockexplorer.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@Component
+public class DataSourceBean {
+
+  @Value("${DATABASE_PASSWORD}")
+  private String password;
+
+  @Value("${DATABASE_USERNAME}")
+  private String username;
+
+  @Bean
+  @Primary
+  public DataSource dataSource() {
+    return DataSourceBuilder
+        .create()
+        .username(username)
+        .password(password)
+        .url("jdbc:oracle:thin:@localhost:51521/XEPDB1")
+        .build();
+  }
+}
