5037b914603c984de62d767e1b7e8416b3042947
Julian Riegraf <julianriegraf@web.de>
2020-12-31 16:45:33 +0100

    update install script

--- server/setup.sh
+++ server/setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 apt update
-apt install nginx
+apt install -y nginx pwgen
 
 # check if docker is available
 if ! command -v docker &> /dev/null
 then
+    echo "--- Installing docker ---"
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
 fi
@@ -13,18 +14,25 @@ fi
 # check if docker-compose is available
 if ! command -v docker-compose &> /dev/null
 then
+    echo "--- Installing docker-compose ---"
     curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
 curl -L -o docker-compose.yml https://raw.githubusercontent.com/jriegraf/lock-explorer/main/server/docker-compose.yml
 curl -L -o create_user.sql https://raw.githubusercontent.com/jriegraf/lock-explorer/main/server/create_user.sql
-curl -L -o .env https://raw.githubusercontent.com/jriegraf/lock-explorer/main/server/create_user.sql
-
-
+curl -L -o .env https://raw.githubusercontent.com/jriegraf/lock-explorer/main/server/.env
 
 # Update all images
-docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
+# docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
 
+ORACLE_PASSWORD="$(pwgen 14 1)"
+DATABASE_PASSWORD="$(pwgen 14 1)"
 
-# docker-compose up --no-recreate
+sed -i "s/<<ORACLE_PASSWORD>>/$ORACLE_PASSWORD/g" .env
+sed -i "s/<<ORACLE_PASSWORD>>/$DATABASE_PASSWORD/g" .env
+
+echo "--- .env file ---"
+cat .env
+
+docker-compose up
